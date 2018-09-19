55f37ac3f6e83f13d63c83b86857dfa8b1665e38
Carlos Jaimes <info@fixcomputing.com>
2018-09-19 14:25:52 -0400

    new changes

--- react-redux-client/src/components/Todo.js
+++ react-redux-client/src/components/Todo.js
@@ -56,18 +56,18 @@ export default class Todo extends React.Component {
             <h2>Files</h2>
 
             {todoState.todo.path1 != 'undefined' &&
-            <img src={"/var/www/reactApp/express-server/public/" + todoState.todo.path1} height="200" width="200"/>
+            <img src={"http://localhost:3001/public/" + todoState.todo.path1} height="200" width="200"/>
             // <img src={"/Users/Carlos/Repositorios/ReactApp/express-server/public/" + todoState.todo.path1} height="200" width="200"/>
             }
 
 
             {todoState.todo.path2 != 'undefined' &&
-            <img src={"/var/www/reactApp/express-server/public/" + todoState.todo.path2} height="200" width="200"/>
+            <img src={"http://localhost:3001/public/" + todoState.todo.path2} height="200" width="200"/>
             // <img src={"/Users/Carlos/Repositorios/ReactApp/express-server/public/" + todoState.todo.path2} height="200" width="200"/>
             }
 
             {todoState.todo.path3 != 'undefined' &&
-            <img src={"/var/www/reactApp/express-server/public/" + todoState.todo.path3} height="200" width="200"/>
+            <img src={"http://localhost:3001/public/" + todoState.todo.path3} height="200" width="200"/>
             // <img src={"/Users/Carlos/Repositorios/ReactApp/express-server/public/" + todoState.todo.path3} height="200" width="200"/>
             }
 
